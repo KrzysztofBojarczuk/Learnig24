f6b5823e48383ff442e11120d85ce1621c5cbec5
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-02-23 18:26:37 +0100

    Create zwrotimienia.cs

---
+++ 1/zwrotimienia.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj imie: ");
+            string imieUzytkownika = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine($"Witaj {imieUzytkownika}!");
+
+
+        }
+            
+    }
+}
