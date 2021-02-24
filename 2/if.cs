b61662105176aa5b7dc0762109e52235e2ac8e36
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-02-24 15:19:53 +0100

    Create if.cs

---
+++ 2/if.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool pierwszaZmiennaTyouBoolean = false;
+            bool drugaZmeinnaTyouBoolean = false;
+            bool trzeciaZmiennaTypuboolean = false;
+          
+
+
+            if (pierwszaZmiennaTyouBoolean)
+            {
+                Console.WriteLine("Pierwsza zmienna jest warta true");
+            }
+            else if (drugaZmeinnaTyouBoolean)
+            {
+                Console.WriteLine("Druga zmienna jest warta false");
+            }
+            else if (trzeciaZmiennaTypuboolean) 
+            {
+                Console.WriteLine("Trzecia zmeinna jest warta true");
+            }
+            else
+            {
+                Console.WriteLine("No to ja juz nie wiem co zrobić...");
+            }
+        }
+    }
+}
